e1a70394a2758af20d4468a0e328dd3b46e06798
Faraz Khan <fk00786000@gmail.com>
2022-07-26 11:34:27 +0530

    bootstrap rehta h

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
+  resources :user_stocks, only: [:create, :destroy]
   devise_for :users
-  devise_scope :user do
-    get '/users/sign_out' => 'devise/sessions#destroy'
-  end
   root 'welcome#index'
-  
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get 'my_portfolio', to: 'users#my_portfolio'
+  get 'search_stock', to: 'stocks#search'
+  get 'my_friends', to: 'users#my_friends'
+  get 'search_friend', to: 'users#search'
+  resources :friendships, only: [:create, :destroy]
+  resources :users, only: [:show]
 end
